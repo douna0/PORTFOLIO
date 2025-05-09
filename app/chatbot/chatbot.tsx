77f6a4fccfc6c79726d6238be4f321dae06310ea
mini <projet>
2025-05-09 13:55:12 +0200

    saving all changes till now

--- app/chatbot/chatbot.tsx
+++ app/chatbot/chatbot.tsx
@@ -29,7 +29,7 @@ try {
     <div className='p-4 max-w-xl mx-auto'>
         <h2 className='text-xl font-bold mb-2'> HananeBot</h2>
         <form onSubmit={handleSubmit} className='mb-4'>
-            <input type="text" className='border p-2 w-full mb-2' placeholder='Ask your question about me ...' value={question} onChange={(e)=>setQuestion(e.target.value)}/>
+            <input type="text" className='border border-b-cyan-500 p-2 w-full mb-2' placeholder='Ask your question about me ...' value={question} onChange={(e)=>setQuestion(e.target.value)}/>
             <button type="submit" className='bg-blue-600 text-white px-4 py-2 rounded-b-3xl'>Send</button>
             {response && (
                 <div className='bg-gray-100 p-3 rounded'>
